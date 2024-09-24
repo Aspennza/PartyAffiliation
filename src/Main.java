fde77588af3b67b4908b5350154ea25a42d1b562
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 11:44:55 -0400

    Program complete--changed input println to print for readability & removed comments

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //String partyAffiliation = “”
         Scanner in = new Scanner(System.in);
         String partyAffiliation = "";
         String menu = "D - Democrat\tR - Republican\tI - Independent\n\nEnter your party affiliation: ";
 
-        //output “Enter your party affiliation: ”
         System.out.print(menu);
 
-        //input partyAffiliation
         partyAffiliation = in.nextLine();
 
         if (partyAffiliation.equalsIgnoreCase("D"))
@@ -31,15 +28,5 @@ public class Main
         {
             System.out.println("\nYou entered a party affiliation other than D, R, or I, so you get Other. You entered: " + partyAffiliation);
         }
-
-        //if partyAffiliation == “D” then
-        //output “You get a Democratic Donkey.”
-        //else if partyAffiliation == “R” then
-        //output “You get a Republican Elephant.”
-        //else if partyAffiliation == “I” then
-        //output “You get an Independent Man.”
-        //else
-        //output “You did not enter a recognizable party affiliation. Enter D, R, or I. You entered: ” + partyAffiliation
-        //endIf
     }
 }
